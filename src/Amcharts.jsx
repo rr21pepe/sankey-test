a7580087186d93180ce212f2ab4474256cadbc8e
rr21pepe <pepe@redradix.com>
2023-05-09 14:01:30 +0200

    Add props to pass data and connexions

--- src/Amcharts.jsx
+++ src/Amcharts.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import * as am5 from "@amcharts/amcharts5";
 import * as am5flow from "@amcharts/amcharts5/flow";
 
-export default function AmchartsSenkey() {
+export default function AmchartsSenkey({ data, connexions }) {
   const ref = useRef(null);
 
   useLayoutEffect(() => {
@@ -46,30 +46,8 @@ export default function AmchartsSenkey() {
     series.nodes.rectangles.template.set("templateField", "nodeSettings");
     series.links.template.set("templateField", "linkSettings");
 
-    series.nodes.data.setAll([
-      { id: "A", name: "Node A", value: 200, fill: am5.color(0xff621f) },
-      { id: "B", name: "Node B", value: 201, fill: am5.color(0x297373) },
-      { id: "C", name: "Node C", value: 202, fill: am5.color(0xff621f) },
-      { id: "D", name: "Node D", value: 203, fill: am5.color(0x946b49) },
-      { id: "E", name: "Node E", value: 204, fill: am5.color(0x297373) },
-      { id: "G", name: "Node G", value: 205, fill: am5.color(0xff621f) },
-      { id: "H", name: "Node H", value: 206, fill: am5.color(0x297373) },
-      { id: "I", name: "Node I", value: 207, fill: am5.color(0x946b49) },
-      { id: "J", name: "Node J", value: 208, fill: am5.color(0x297373) },
-    ]);
-    series.data.setAll([
-      { from: "A", to: "D", value: 100 },
-      { from: "B", to: "D", value: 80 },
-      { from: "B", to: "E", value: 40 },
-      { from: "C", to: "E", value: 30 },
-      { from: "D", to: "G", value: 50 },
-      { from: "D", to: "I", value: 20 },
-      { from: "D", to: "H", value: 30 },
-      { from: "E", to: "H", value: 60 },
-      { from: "G", to: "J", value: 50 },
-      { from: "I", to: "J", value: 10 },
-      { from: "H", to: "J", value: 90 },
-    ]);
+    series.nodes.data.setAll(data);
+    series.data.setAll(connexions);
 
     series.appear(1000, 100);
 
--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import * as am5 from "@amcharts/amcharts5";
+
 import GoogleCharts from "./GoogleCharts";
 import Plotly from "./Plotly";
 import AmchartsSenkey from "./Amcharts";
@@ -7,7 +9,32 @@ function App() {
   return (
     <div className="App">
       {/* <GoogleCharts /> */}
-      <AmchartsSenkey />
+      <AmchartsSenkey
+        data={[
+          { id: "A", name: "Node A", value: 200, fill: am5.color(0xff621f) },
+          { id: "B", name: "Node B", value: 201, fill: am5.color(0x297373) },
+          { id: "C", name: "Node C", value: 202, fill: am5.color(0xff621f) },
+          { id: "D", name: "Node D", value: 203, fill: am5.color(0x946b49) },
+          { id: "E", name: "Node E", value: 204, fill: am5.color(0x297373) },
+          { id: "G", name: "Node G", value: 205, fill: am5.color(0xff621f) },
+          { id: "H", name: "Node H", value: 206, fill: am5.color(0x297373) },
+          { id: "I", name: "Node I", value: 207, fill: am5.color(0x946b49) },
+          { id: "J", name: "Node J", value: 208, fill: am5.color(0x297373) },
+        ]}
+        connexions={[
+          { from: "A", to: "D", value: 100 },
+          { from: "B", to: "D", value: 80 },
+          { from: "B", to: "E", value: 40 },
+          { from: "C", to: "E", value: 30 },
+          { from: "D", to: "G", value: 50 },
+          { from: "D", to: "I", value: 20 },
+          { from: "D", to: "H", value: 30 },
+          { from: "E", to: "H", value: 60 },
+          { from: "G", to: "J", value: 50 },
+          { from: "I", to: "J", value: 10 },
+          { from: "H", to: "J", value: 90 },
+        ]}
+      />
       {/* <Plotly /> */}
     </div>
   );
